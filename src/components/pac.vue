b63afdf37dd209177629402b08635ae6c9441d51
ZhangKaitlyn <1873070425@qq.com>
2018-05-05 18:38:13 +0800

    fix

--- src/components/pac.vue
+++ src/components/pac.vue
@@ -149,7 +149,7 @@
               <img src="@/assets/pac/paracraft_topmenu_logo.png" alt="">
               <div class="paracraft-intro">
                 <p>
-                  <span>Paracraft 创意空间</span>是一款面向7岁以上用户的免费的3D创作软件。</p>
+                  <span class="stress">Paracraft 创意空间</span>是一款面向7岁以上用户的免费的3D创作软件。</p>
                 <p>你可以用它创建3D场景和人物，制作动画和电影，学习和编写计算机程序。下载软件后，</p>
                 <p>可观看百部优秀的用户电影作品，创造属于你的个人作品。</p>
               </div>
@@ -880,6 +880,9 @@ body {
     }
     .more {
       padding-left: 26px;
+    }
+    .stress{
+      font-weight: bold;
     }
     .paracraft-info {
       text-align: center;
