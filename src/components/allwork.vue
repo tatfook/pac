d03316774cc913dd3d832e43c299e97b3104a69c
ZhangKaitlyn <1873070425@qq.com>
2018-05-18 19:04:52 +0800

    参赛作品搜索

--- src/components/allwork.vue
+++ src/components/allwork.vue
@@ -11,9 +11,55 @@
         <h1 class="title">
           <img src="@/assets/pac/all_works_title.png" alt="">
         </h1>
+        <div class="search-row clearfix">
+          <div class="search-item">
+            <span class="label">组别：</span>
+            <el-dropdown @command="setSelectWorksFlag" class="flag-select search-dropdown">
+              <span class="el-dropdown-link">
+                {{selectWorksFlag}}
+                <img class="dropdown-icon" src="@/assets/pac/bottom-dot.png" alt="">
+              </span>
+              <el-dropdown-menu slot="dropdown">
+                <el-dropdown-item>全部</el-dropdown-item>
+                <el-dropdown-item :command=3>公开组</el-dropdown-item>
+                <el-dropdown-item :command=4>学生组</el-dropdown-item>
+              </el-dropdown-menu>
+            </el-dropdown>
+          </div>
+          <div class="search-item">
+            <span class="label">奖项类别筛选：</span>
+            <el-dropdown @command="setSelectAward" class="award-select search-dropdown">
+              <span class="el-dropdown-link">
+                {{searchParams.awords || '全部'}}
+                <img class="dropdown-icon" src="@/assets/pac/bottom-dot.png" alt="">
+              </span>
+              <el-dropdown-menu slot="dropdown" class="awards-dropdown-menu">
+                <el-dropdown-item v-for="(award, index) in usableAwards" :key="index" :command=award>{{award}}</el-dropdown-item>
+              </el-dropdown-menu>
+            </el-dropdown>
+          </div>
+          <div class="search-item">
+            <span class="label">排序：</span>
+            <el-dropdown @command="setSelectOrder" class="order-select search-dropdown">
+              <span class="el-dropdown-link">
+                {{selectOrder}}
+                <img class="dropdown-icon" src="@/assets/pac/bottom-dot.png" alt="">
+              </span>
+              <el-dropdown-menu slot="dropdown">
+                <el-dropdown-item v-for="(order, index) in usableOrders" :key="index" :command=order>{{order.text}}</el-dropdown-item>
+              </el-dropdown-menu>
+            </el-dropdown>
+          </div>
+          <div class="search-item">
+            <div class="input-group">
+              <input v-model="searchParams.keyword" type="text" placeholder="请输入作品名称/编号/作者">
+              <span class="iconfont icon-search" @click="doSearch"></span>
+            </div>
+          </div>
+        </div>
         <div class="works">
           <div class="work-item" v-for="(work, index) in allWorksArr" :key='index'>
-            <span class="id-label">{{work._id}}</span>
+            <span class="id-label">{{work.worksId}}</span>
             <img class="cover" :src="work.worksLogo" alt="">
             <h2>{{work.worksName || '暂无标题'}}</h2>
             <p class="time">{{work.updateDate || '时间未知'}}</p>
@@ -48,6 +94,32 @@ import Header from './common/header'
 import Banner from './common/banner'
 import Footer from './common/footer'
 const iiccWebsiteId = process.env.IICC_WEBSITE_ID
+const Awards = {
+  default: {
+    key: undefined,
+    text: '全部'
+  },
+  '3': [
+    'NPL 大奖',
+    'NPL 最佳教程奖',
+    'NPL 最佳编程奖',
+    'NPL 最佳场景设计奖',
+    'NPL 最佳3D角色奖',
+    'NPL 最佳开源贡献奖',
+    'NPL 老顽童奖',
+    'NPL 小小梦想家',
+    'NPL 人气十佳'
+  ],
+  '4': [
+    'NPL 最佳故事短片奖',
+    'NPL 优秀游戏作品奖',
+    'NPL 优秀材质奖',
+    'NPL 优秀视觉特效奖',
+    'NPL 优秀原创音效奖',
+    'NPL 优秀剧本创意奖',
+    'NPL 人气十佳'
+  ]
+}
 export default {
   name: 'allwork',
   components: {
@@ -59,11 +131,41 @@ export default {
     return {
       userinfo: JSON.parse(localStorage.getItem('userinfo')),
       allWorksArr: [],
-      worksCount: 0
+      worksCount: 0,
+      usableOrders: [
+        {
+          key: 'createDate',
+          text: '最新上传',
+          commondValue: { createDate: 'desc' }
+        },
+        {
+          key: 'visitCount',
+          text: '最多浏览',
+          commondValue: { visitCount: 'desc' }
+        },
+        {
+          key: 'voteCount',
+          text: '最多投票',
+          commondValue: { voteCount: 'desc' }
+        }
+      ],
+      selectOrder: '',
+      searchParams: {
+        websiteId: iiccWebsiteId,
+        worksFlag: undefined,
+        awords: '',
+        order: {
+          createDate: 'desc'
+        },
+        keyword: ''
+      }
     }
   },
   mounted() {
-    this.getAllWorks()
+    this.doSearch()
+    this.searchParams.awords = this.usableAwards[0].key
+    this.searchParams.order = this.usableOrders[0].commondValue
+    this.selectOrder = this.usableOrders[0].text
   },
   methods: {
     reGetUserinfo() {
@@ -73,12 +175,36 @@ export default {
       this.userinfo = undefined
       localStorage.removeItem('userinfo')
     },
-    getAllWorks() {
+    setSelectWorksFlag(value) {
+      if (value === this.searchParams.worksFlag) {
+        return
+      }
+      switch (value) {
+        case 3:
+        case 4:
+          this.searchParams.worksFlag = value
+          break
+        default:
+          this.searchParams.worksFlag = undefined
+          break
+      }
+      this.searchParams.awords = this.usableAwards[0].key
+      this.doSearch()
+    },
+    setSelectAward(value) {
+      this.searchParams.awords = value === '全部' ? undefined : value
+      this.doSearch()
+    },
+    setSelectOrder(order) {
+      this.searchParams.order = order.commondValue
+      this.selectOrder = order.text
+      this.doSearch()
+    },
+    doSearch() {
       keepwork.websiteWorks
-        .search({
-          websiteId: iiccWebsiteId
-        })
+        .search(this.searchParams)
         .then(result => {
+          console.log(result)
           if (result.data) {
             this.allWorksArr = result.data.list
             this.worksCount = result.data.total
@@ -87,6 +213,39 @@ export default {
         .catch(error => {
           console.log(error)
         })
+    }
+  },
+  computed: {
+    selectWorksFlag() {
+      let selectWorksFlagText = ''
+      switch (this.searchParams.worksFlag) {
+        case 3:
+          selectWorksFlagText = '公开组'
+          break
+        case 4:
+          selectWorksFlagText = '学生组'
+          break
+        default:
+          selectWorksFlagText = '全部'
+          break
+      }
+      return selectWorksFlagText
+    },
+    usableAwards() {
+      let worksFlag = this.searchParams.worksFlag
+      let result = []
+      switch (worksFlag) {
+        case 3:
+        case 4:
+          result = [Awards['default'].text].concat(Awards[worksFlag])
+          break
+        default:
+          result = [Awards['default'].text]
+            .concat(Awards['3'])
+            .concat(Awards['4'])
+          break
+      }
+      return result
     }
   }
 }
@@ -222,6 +381,71 @@ h2 {
   color: #303133;
   margin: 14px 13px 9px;
 }
+.search-row {
+  margin: 24px 0 36px;
+  text-align: center;
+}
+.search-item {
+  display: inline-block;
+  margin-right: 32px;
+  .input-group {
+    background-color: #303133;
+    border: 2px solid;
+    input {
+      height: 34px;
+      border: none;
+      padding: 0 13px;
+      background-color: #eee;
+    }
+    input:focus {
+      outline: none;
+    }
+    .iconfont {
+      color: #fff;
+      display: inline-block;
+      width: 48px;
+      margin: 0;
+      text-align: center;
+      font-size: 26px;
+    }
+  }
+}
+.search-item:last-child {
+  margin-right: 0;
+}
+.search-dropdown {
+  text-align: left;
+  font-size: 13px;
+  padding: 0 13px;
+  background-color: #eee;
+  color: #606266;
+  height: 38px;
+  line-height: 38px;
+  box-sizing: border-box;
+  position: relative;
+  .el-dropdown-link {
+    display: inline-block;
+    min-width: 100%;
+  }
+}
+.dropdown-icon {
+  position: absolute;
+  right: 13px;
+  top: 16px;
+}
+.flag-select {
+  min-width: 88px;
+}
+.award-select {
+  min-width: 165px;
+}
+.order-select {
+  min-width: 100px;
+}
+.awards-dropdown-menu {
+  max-height: 250px;
+  overflow: auto;
+}
 .time {
   font-size: 12px;
   color: #909399;
@@ -243,13 +467,18 @@ h2 {
 .works {
   display: flex;
   flex-wrap: wrap;
-  justify-content: space-between;
+  align-items: flex-start;
 }
 .work-item {
   width: 300px;
   border: 2px solid #f5f5f5;
   margin-bottom: 40px;
+  margin: 0 40px 40px 0;
   position: relative;
+  box-sizing: border-box;
+}
+.work-item:nth-child(3n+0){
+  margin-right: 0;
 }
 .id-label {
   position: absolute;
