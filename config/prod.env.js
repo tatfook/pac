31430bfbdca321e1ebe96faf6bed937f3bcc3b79
ZhangKaitlyn <1873070425@qq.com>
2018-06-29 11:45:24 +0800

    force https

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,7 +1,7 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  KEEPWORK_API_PREFIX: '"http://keepwork.com/api/wiki/models"',
-  GITLAB_API_PREFIX: '"http://git.keepwork.com"',
+  KEEPWORK_API_PREFIX: '"https://keepwork.com/api/wiki/models"',
+  GITLAB_API_PREFIX: '"https://git.keepwork.com"',
   IICC_WEBSITE_ID: '4677'
 }
